81d852957aa4e7471392d4e8b0e833b8c3f07cb3
Darshil1001 <darshilpatel11001@gmail.com>
2024-02-11 14:30:43 -0500

    Initial Commit

---
+++ Assignment_Lab5_MultiPageUITests/Assignment_Lab5_MultiPageUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Assignment_Lab5_MultiPageUITestsLaunchTests.swift
+//  Assignment_Lab5_MultiPageUITests
+//
+//  Created by user238116 on 2/10/24.
+//
+
+import XCTest
+
+final class Assignment_Lab5_MultiPageUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
